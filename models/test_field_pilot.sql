dfa873ad3f6d183af457d1c8643cefac03401f9c
riyanshchouhan-pixel <riyansh.chouhan@headout.com>
2026-05-06 21:03:37 +0530

    Point test model at segment-data.dbt_audit_reporting + gitignore

--- models/test_field_pilot.sql
+++ models/test_field_pilot.sql
@@ -1,9 +1,11 @@
--- Minimal test model. Selects a few columns from an existing
--- table so the model is "valid" without us needing to run dbt.
+{{ config(materialized='view') }}
+
 select
     combined_entity_id,
     report_date,
     sum_revenue_predicted,
     sum_insider_commission
-from `headout-analytics.analytics_reporting.combined_entity_stats`
-where report_date = current_date()
+from `segment-data.dbt_audit_reporting.combined_entity_stats`
+where report_date >= current_date() - 7
+
+
