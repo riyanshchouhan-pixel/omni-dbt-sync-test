822e1be4c468838c510ea511eae0e25d4358dbcd
Riyansh Chouhan <riyansh.chouhan@headout.com>
2026-05-07 11:44:11 +0530

    Update test_field_pilot.sql

--- models/test_field_pilot.sql
+++ models/test_field_pilot.sql
@@ -1,11 +1,1 @@
-{{ config(materialized='view') }}
-
-select
-    combined_entity_id,
-    report_date,
-    sum_revenue_predicted,
-    sum_insider_commission
-from `segment-data.dbt_audit_reporting.combined_entity_stats`
-where report_date >= current_date() - 7
-
 
