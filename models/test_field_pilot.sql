13e74f88125bb1dc61955175fb82e82a8e81dbf0
Riyansh Chouhan <riyansh.chouhan@headout.com>
2026-05-13 07:08:27 +0530

    Update test_field_pilot.sql

--- models/test_field_pilot.sql
+++ models/test_field_pilot.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 
-select 1 as test_id, 'Amsterdam' as city_name, 100.50 as test_revenue
+select 1 as test_id, 'Amsterdam' as city_name, 100.50 as test_revenue, 'Netherlands' as country_name
 union all
-select 2 as test_id, 'Paris' as city_name, 250.75 as test_revenue
+select 2 as test_id, 'Paris' as city_name, 250.75 as test_revenue, 'France' as country_name
 union all
-select 3 as test_id, 'Tokyo' as city_name, 500.00 as test_revenue
+select 3 as test_id, 'Tokyo' as city_name, 500.00 as test_revenue, 'Japan' as country_name
